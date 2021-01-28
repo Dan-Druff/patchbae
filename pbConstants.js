e576cde028c25b81cd743d3eb3dbdf0fe2a6dba2
Dan Druff <hamtron2000@gmail.com>
2021-01-28 14:50:18 -0500

    Going to get NavBar

--- pbConstants.js
+++ pbConstants.js
@@ -1,1 +1,10 @@
 console.log("HI FROM PBconstatnts");
+
+var brownSort = new Patch("brown", 0, 0, "brown", "");
+var redSort = new Patch("red", 0, 0, "red", "");
+var orangeSort = new Patch("orange", 0, 0, "orange", "");
+var yellowSort = new Patch("yellow", 0, 0, "yellow", "");
+var greenSort = new Patch("green", 0, 0, "green", "");
+var blueSort = new Patch("blue", 0, 0, "blue", "");
+var noneSort = new Patch("none", 0, 0, "none", "");
+var workingObject = new Patch("working", 0, 0, "working", "");
--- pbPatch.js
+++ pbPatch.js
@@ -1,1 +1,86 @@
 console.log("HI FROM PBPATCH");
+
+// var currentDate = new Date();
+
+
+function Patch(user, inputs, outputs, name, notes){
+    
+    this.user = user;
+    this.inputs = inputs;
+    this.outputs = outputs;
+    this.name = name;
+    this.notes = notes;
+    this.id = "";
+    this.inChNumArray = [];
+    this.inChNameArray = [];
+    this.inChColorArray = [];
+    this.inChPatchArray = [];
+    this.inChMicArray = [];
+    this.inChStandArray = [];
+    this.inChNotesArray = [];
+    this.outChNumArray = [];
+    this.outChNameArray = [];
+    this.outChColorArray = [];    
+    this.outChPatchArray = [];
+    this.outChNotesArray = [];
+    this.inputBool = true;
+    this.selectedChannel = 0;
+    this.selectedColor = "white";
+    this.patchBool1 = false;
+    this.patchBool2 = false;
+
+    let currentDate = new Date();
+    this.dateCreated = currentDate;
+    this.dateModified = currentDate;
+
+    //CREATE INPUT ARRAYS
+    for(i=0; i<inputs; i++){
+        this.inChNumArray.push("");
+        this.inChNameArray.push("Mario");
+        this.inChColorArray.push("");
+        this.inChMicArray.push("");
+        this.inChPatchArray.push("");
+        this.inChStandArray.push("");
+        this.inChNotesArray.push("");
+    }
+    console.log("Finished input for loop" + this.inChNameArray);
+
+    //CREATE OUTPUT ARRAys
+    for (o=0; o<outputs; o++){
+        this.outChNumArray.push("");
+        this.outChNameArray.push("Luigi");
+        this.outChColorArray.push("");
+        this.outChPatchArray.push("");
+        this.outChNotesArray.push("");
+
+    }
+    console.log("Finsiehd output for loop " + this.outChNameArray);
+}
+
+
+Patch.prototype.updateInput = function(intel){
+    console.log("Calling prototype function update Input, do something with *this* and intel: " + intel);
+}
+Patch.prototype.updateOutput = function(intel){
+    console.log("Calling prototype function update Input, do something with *this* and intel: " + intel);
+}
+Patch.prototype.checkForErrors = function(){
+    console.log("Checking for errors prototype function");
+}
+Patch.prototype.clearPatch = function(){
+    console.log("Clear Patch Function");
+    this.inputs = 0;
+    this.outputs = 0;
+    this.inChNumArray = [];
+    this.inChNameArray = [];
+    this.inChColorArray = [];
+    this.inChPatchArray = [];
+    this.inChMicArray = [];
+    this.inChStandArray = [];
+    this.inChNotesArray = [];
+    this.outChNumArray = [];
+    this.outChNameArray = [];
+    this.outChColorArray = [];    
+    this.outChPatchArray = [];
+    this.outChNotesArray = [];
+}
